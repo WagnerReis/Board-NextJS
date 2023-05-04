3d794a49d703ee82024a0f8d43dafa32a38bd66c
WagnerReis <wagnerreis1997@gmail.com>
2023-05-04 08:27:36 -0300

    Adicionando Sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import styles from "../styles/styles.module.scss";
+
 export default function Home() {
   return (
     <div>
-      <h1>Primeiro projeto com Nextjs</h1>
+      <h1 className={styles.title}>
+        Primeiro projeto com <span>NextJS</span>
+      </h1>
     </div>
-  )
+  );
 }
+ 
