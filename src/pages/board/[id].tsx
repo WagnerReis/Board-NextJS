e67cc5fae3e4b0a50c3c5edc895528041b5ed517
WagnerReis <wagnerreis1997@gmail.com>
2023-05-12 07:34:59 -0300

    Pagina detalhes parte 2

--- src/pages/board/[id].tsx
+++ src/pages/board/[id].tsx
@@ -4,27 +4,42 @@ import { getSession } from "next-auth/react";
 import firebase from "../../services/firebaseConnection";
 import { format } from "date-fns";
 
-type Task = {
+import styles from "./task.module.scss";
+import { FiCalendar } from "react-icons/fi";
+import Head from "next/head";
+
+type TaskType = {
   id: string;
   created: string | Date;
   createdFormated?: string;
   tarefa: string;
   userId: string;
   nome: string;
-}
+};
 
 interface TaskListProps {
   data: string;
 }
 
 export default function Task({ data }: TaskListProps) {
-  const task: Task = JSON.parse(data);
+  const task: TaskType = JSON.parse(data);
 
   return (
-    <div>
-      <h1>Pagina detalhes</h1>
-      <h2>{task.tarefa}</h2>
-    </div>
+    <main className={styles.container}>
+      <Head>
+        <title>Detalhes da sua tarefa</title>
+      </Head>
+      <article className={styles.container}>
+        <div className={styles.actions}>
+          <div>
+            <FiCalendar size={20} color="#FFB800" />
+            <span>Tarefa criada: </span>
+            <time>{task.createdFormated}</time>
+          </div>
+        </div>
+        <p>{task.tarefa}</p>
+      </article>
+    </main>
   );
 }
 
@@ -35,21 +50,26 @@ export const getServerSideProps: GetServerSideProps = async ({
   const { id } = params;
   const session = await getSession({ req });
 
-  const data = await firebase.firestore().collection('tarefas')
-  .doc(String(id))
-  .get()
-  .then((snapshot) => {
-    const data = {
-      id: snapshot.id,
-      created: snapshot.data()?.created,
-      createdFormated: format(snapshot.data()?.created.toDate(), 'dd MMMM yyyy'),
-      tarefa: snapshot.data()?.tarefa,
-      userId: snapshot.data()?.userId,
-      nome: snapshot.data()?.nome
-    }
+  const data = await firebase
+    .firestore()
+    .collection("tarefas")
+    .doc(String(id))
+    .get()
+    .then((snapshot) => {
+      const data = {
+        id: snapshot.id,
+        created: snapshot.data()?.created,
+        createdFormated: format(
+          snapshot.data()?.created.toDate(),
+          "dd MMMM yyyy"
+        ),
+        tarefa: snapshot.data()?.tarefa,
+        userId: snapshot.data()?.userId,
+        nome: snapshot.data()?.nome,
+      };
 
-    return JSON.stringify(data)
-  })
+      return JSON.stringify(data);
+    });
 
   if (!session?.id) {
     return {
@@ -62,7 +82,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   return {
     props: {
-      data
+      data,
     },
   };
 };
